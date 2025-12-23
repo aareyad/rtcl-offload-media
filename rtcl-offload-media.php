e68075eb8cf380743024e472c882022e9bd46e5c
aareyad <aliakbar.reyad@gmail.com>
2025-12-23 18:29:32 +0600

    added licensing details

--- rtcl-offload-media.php
+++ rtcl-offload-media.php
@@ -7,6 +7,8 @@
  * Version: 1.0.0
  * Author: RadiusTheme
  * Author URI: https://radiustheme.com
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: rtcl-offload-media
  * Domain Path: /languages
  *
